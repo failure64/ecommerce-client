be1ca635dfa1fe12ae405bfa98230288b4fc593e
failure64 <swastikmiddha@gmail.com>
2023-08-01 11:08:52 +0530

    Individual product and category card

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import { Urbanist } from 'next/font/google'
 
-
+import ModalProvider from '@/providers/modal-provider'
+import ToastProvider from '@/providers/toast-provider'
+import Navbar from '@/components/navbar'
+import Footer from '@/components/footer'
 
 import './globals.css'
 
@@ -19,7 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        
+        <ToastProvider />
+        <ModalProvider />
+        <Navbar />
+        {children}
+        <Footer />
       </body>
     </html>
   )
